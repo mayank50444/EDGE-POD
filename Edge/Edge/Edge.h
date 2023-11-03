b93de499596c654462c983fd9f3df41120f6b839
Mayank Dubey <mayank.dubey@housing.com>
2023-11-03 17:20:29 +0530

    Added framework source and podspec

---
+++ Edge/Edge/Edge.h
@@ -0,0 +1,18 @@
+//
+//  Edge.h
+//  Edge
+//
+//  Created by Mayank Dubey on 18/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Edge.
+FOUNDATION_EXPORT double EdgeVersionNumber;
+
+//! Project version string for Edge.
+FOUNDATION_EXPORT const unsigned char EdgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Edge/PublicHeader.h>
+
+
