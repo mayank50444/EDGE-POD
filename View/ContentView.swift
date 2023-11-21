b3c50e4e93365f04d68eac3c3ca560f886ce5644
GopalHousing22 <108732246+GopalHousing22@users.noreply.github.com>
2023-11-21 14:01:29 +0530

    navigations added

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -49,7 +49,7 @@ struct QnATagsDatum: Codable, Hashable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
-
+    
     enum CodingKeys: String, CodingKey {
         case id, name
         case displayName = "display_name"
@@ -61,68 +61,80 @@ public struct Content: View {
     public init(myEdgeFrameworkInstance: MyEdgeFrameworkClass) {
         self.myEdgeFrameworkInstance = myEdgeFrameworkInstance
     }
-    
-     public var body: some View {
-         VStack {
-             Button(action: {myEdgeFrameworkInstance.doSomething()}) {
-               Text("Border Button")
-                 .padding()
-                 .border(.blue)
-             }
-
-             let endPoint = Endpoint<QnATagsModel>(path: "odin/data/api/v1/master-tags",
-                             method: .get,
-                             domain: "odin")
-             
-             var apiDataNetworkConfig : ApiDataNetworkConfig?
-             
-             var globalHeaders : [String:String] {
-                 get {
-                     let version = (Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "")
-                     let buildNo = (Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "")
-                     var header = ["Content-Type":"application/json",
-                                   "User-Agent":"Native/ios",
-                                   "app_name":Bundle.main.bundleIdentifier ?? "",
-                                   "app_version": "\(version) (\(buildNo))",
-                                   //                      "js_bundle_version": "",
-                                   "client_id": UIDevice.current.identifierForVendor?.uuidString ?? ""]
-                     
-                     return header
-                 }
-             }
-             
-             var apiFailureInterceptor : (_ apiFailureResponse : Any?,_ statusCode : Int, _ isErrorHandlingBeingDoneByClient: Bool, _ urlRequest : URLRequest?) -> () = { apiFailureResponse, statusCode, isErrorHandlingBeingDoneByClient, urlRequest in
-             }
-             
-             var networkAndOtherErrorIntercepter : ((DataTransferError, Bool, URLRequest?) -> ()) = { error, isErrorHandlingBeingDoneByClient, urlRequest in
-                 var message = ""
-             }
-             
-             var apiDataTransferService: DataTransferService = {
-                 apiDataNetworkConfig = ApiDataNetworkConfig(baseURL: URL(string: "https://housing.com")!,
-                                                             headers: globalHeaders,
-                                                   queryParameters: [:])
-                 let apiDataNetwork = DefaultNetworkService(config: apiDataNetworkConfig!)
-                 return DefaultDataTransferService(with: apiDataNetwork, apiFailureInterceptor: apiFailureInterceptor, networkAndOtherErrorIntercepter: networkAndOtherErrorIntercepter)
-             }()
-             
-             Button {
-                 
-                 Task {
-                     let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
-                     apiDataTransferService.request(with: endPoint, apiSuccess: { result in
-                         print(result)
-                     })
-                 }
-             } label: {
-                 Text("Fetch Joke")
-             }
-             Text("Hello, World!")
-            //  Text("Hello, World!")
-            //  Text("Hello, World!")
-            //  Text("Hello, World!")
-            //  Text("Hello, World! Edge")
-         }
+    @Environment(\.presentationMode) var presentationMode
+    @State private var isLinkActive = false
+    public var body: some View {
+        NavigationView {
+            VStack {
+                Button(action: {myEdgeFrameworkInstance.doSomething()}) {
+                    Text("Border Button")
+                        .padding()
+                        .border(.blue)
+                }
+                
+                let endPoint = Endpoint<QnATagsModel>(path: "odin/data/api/v1/master-tags",
+                                                      method: .get,
+                                                      domain: "odin")
+                
+                var apiDataNetworkConfig : ApiDataNetworkConfig?
+                
+                var globalHeaders : [String:String] {
+                    get {
+                        let version = (Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "")
+                        let buildNo = (Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "")
+                        let header = ["Content-Type":"application/json",
+                                      "User-Agent":"Native/ios",
+                                      "app_name":Bundle.main.bundleIdentifier ?? "",
+                                      "app_version": "\(version) (\(buildNo))",
+                                      //                      "js_bundle_version": "",
+                                      "client_id": UIDevice.current.identifierForVendor?.uuidString ?? ""]
+                        
+                        return header
+                    }
+                }
+                
+                let apiFailureInterceptor : (_ apiFailureResponse : Any?,_ statusCode : Int, _ isErrorHandlingBeingDoneByClient: Bool, _ urlRequest : URLRequest?) -> () = { apiFailureResponse, statusCode, isErrorHandlingBeingDoneByClient, urlRequest in
+                }
+                
+                let networkAndOtherErrorIntercepter : ((DataTransferError, Bool, URLRequest?) -> ()) = { error, isErrorHandlingBeingDoneByClient, urlRequest in
+                    var message = ""
+                }
+                
+                let apiDataTransferService: DataTransferService = {
+                    apiDataNetworkConfig = ApiDataNetworkConfig(baseURL: URL(string: "https://housing.com")!,
+                                                                headers: globalHeaders,
+                                                                queryParameters: [:])
+                    let apiDataNetwork = DefaultNetworkService(config: apiDataNetworkConfig!)
+                    return DefaultDataTransferService(with: apiDataNetwork, apiFailureInterceptor: apiFailureInterceptor, networkAndOtherErrorIntercepter: networkAndOtherErrorIntercepter)
+                }()
+                
+                Button {
+                    Task {
+                        let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+                        apiDataTransferService.request(with: endPoint, apiSuccess: { result in
+                            print(result)
+                        })
+                    }
+                } label: {
+                    Text("Fetch Joke")
+                }
+                
+                Text("Hello, World!")
+                
+                Button("Back to UIKit") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                
+                VStack {
+                    Text("click below to navigate to this same screen again")
+                    Button("Navigate Again") {
+                        isLinkActive.toggle()
+                    }
+                    NavigationLink("", destination: Content(myEdgeFrameworkInstance: myEdgeFrameworkInstance), isActive: $isLinkActive)
+                        .hidden()
+                }
+            }
+        }
     }
 }
 
